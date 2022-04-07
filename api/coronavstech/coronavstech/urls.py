556dcc47dfeb8ca6ea8b5406a7422664397cea74
Maxim Mitin <maximus405@gmail.com>
2022-04-08 00:12:29 +0300

    adding new model, update settings for admin page

--- api/coronavstech/coronavstech/urls.py
+++ api/coronavstech/coronavstech/urls.py
@@ -14,16 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from rest_framework import routers
-
-# from companies.urls import companies_router
-from companies.urls import companies_router
-
-# router = routers.DefaultRouter()
-
+from django.urls import path
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", include(companies_router.urls)),
+    path('admin/', admin.site.urls),
 ]
