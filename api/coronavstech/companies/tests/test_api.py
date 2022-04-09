e73c1bc48787c574bda330cca2dc653bd817a1b9
Maxim Mitin <maximus405@gmail.com>
2022-04-09 18:06:23 +0300

    add more test, covers some func

--- api/coronavstech/companies/tests/test_api.py
+++ api/coronavstech/companies/tests/test_api.py
@@ -39,5 +39,32 @@ class TestPostCompanies(BasicCompanyApiTestCase):
         self.assertEqual(response.status_code, 400)
         self.assertEqual(json.loads(response.content), {"name": ["This field is required."]})
 
+    def test_create_exists_company_should_fail(self) -> None:
+        Company.objects.create(name="Amazon")
+        response = self.client.post(path=self.companies_url, data={"name":"Amazon"})
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(json.loads(response.content), {"name": ["company with this name already exists."]})
+
+    def test_create_company_with_only_name_all_fields_should_default(self) -> None:
+        response = self.client.post(path=self.companies_url, data={"name":"SamsungE"})
+        self.assertEqual(response.status_code, 201)
+        response_content = json.loads(response.content)
+        self.assertEqual(response_content.get("name"), "SamsungE")
+        self.assertEqual(response_content.get("status"), "Hiring")
+        self.assertEqual(response_content.get("notes"), "")
+        self.assertEqual(response_content.get("application_link"), "")
+
+    def test_create_company_with_layoffs_status_should_succes(self) -> None:
+        response = self.client.post(path=self.companies_url, data={"name":"Samsung","status":"Lauoffs"})
+        self.assertEqual(response.status_code, 201)
+        response_content = json.loads(response.content)
+        self.assertEqual(response_content.get("status"), "Lauoffs")
+
+    def test_create_company_with_wrong_status_status_should_fail(self) -> None:
+        response = self.client.post(path=self.companies_url, data={"name": "Samsung", "status": "bebra_layoffs"})
+        self.assertEqual(response.status_code, 400)
+        self.assertIn("is not valid", str(response.content))
+
+
 
 
