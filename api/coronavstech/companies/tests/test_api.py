bb4bb2daac3e88f4ae91a121199933e4de2101fb
Maxim Mitin <maximus405@gmail.com>
2022-04-09 17:43:49 +0300

    clean code, add few new classes  and funcs to test

--- api/coronavstech/companies/tests/test_api.py
+++ api/coronavstech/companies/tests/test_api.py
@@ -7,21 +7,24 @@ from django.urls import reverse
 
 from companies.models import Company
 
-
 @pytest.mark.django_db
-class TestGetCompanies(TestCase):
+class BasicCompanyApiTestCase(TestCase):
+    def setUp(self) -> None:
+        self.client = Client()
+        self.companies_url = reverse("companies-list")
+
+    def tearDown(self) -> None:
+        pass
+
+class TestGetCompanies(BasicCompanyApiTestCase):
     def test_zero_companies_should_return_empty_list(self) -> None:
-        client = Client()
-        companies_url = reverse("companies-list")
-        response = client.get(companies_url)
+        response = self.client.get(self.companies_url)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(json.loads(response.content), [])
 
     def test_one_company_exists_should_succed(self) -> None:
-        client = Client()
         amazon = Company.objects.create(name="Amazon")
-        companies_url = reverse("companies-list")
-        response = client.get(companies_url)
+        response = self.client.get(self.companies_url)
         response_content = json.loads(response.content)[0]
         self.assertEqual(response_content.get("name"), amazon.name)
         self.assertEqual(response_content.get("status"), amazon.status)
@@ -30,7 +33,11 @@ class TestGetCompanies(TestCase):
 
         amazon.delete()
 
-
+class TestPostCompanies(BasicCompanyApiTestCase):
+    def test_create_company_without_arguments_should_fail(self) -> None:
+        response = self.client.post(path=self.companies_url, )
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(json.loads(response.content), {"name": ["This field is required."]})
 
 
 
