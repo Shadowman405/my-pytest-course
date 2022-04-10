9328d4c51ab1f180342c9eb6b3746e4dd2bc1959
Maxim Mitin <maximus405@gmail.com>
2022-04-10 18:54:00 +0300

    work with asserts

--- api/coronavstech/companies/tests/test_api.py
+++ api/coronavstech/companies/tests/test_api.py
@@ -73,6 +73,14 @@ class TestPostCompanies(BasicCompanyApiTestCase):
     def test_should_be_ok_if_fails(self) -> None:
         self.assertEqual(1 , 2)
 
+def raise_covid19_exception() -> None:
+    raise ValueError("Corona Exception")
+
+def test_raise_covid19_exception_should_pass() -> None:
+    with pytest.raises(ValueError) as e:
+        raise_covid19_exception()
+    assert "Corona Exception" == str(e.value)
+
 
 
 
